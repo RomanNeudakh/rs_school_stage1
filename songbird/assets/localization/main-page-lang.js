bbce3eb633cfcc04936fae16cf80cb0cc4f4b2fe
Roman Neudakh <romzezzz.nevdah@yandex.ru>
2022-11-18 01:56:29 +0300

    feat: main and gallary pages

---
+++ songbird/assets/localization/main-page-lang.js
@@ -0,0 +1,16 @@
+'use strict'
+
+export let mainLang = {
+  '.nav-link': {
+    'RU': ['Об игре', 'Галерея', 'Результаты'],
+    'EN': ['About', 'Gallery', 'Results']
+  },
+  '.preview-text': {
+    'RU': ['Перед Вами игра-викторина Songbird. Правила игры очень просты! Вам будут представлены 6 вопросов в каждом из которых вам предстоит определить породу птицы по ее пению. Если игрок дал правильный ответ с первой попытки, его счёт увеличивается на 5 баллов, каждая следующая попытка даёт на один балл меньше, если правильный ответ дан только с последней, шестой попытки, игрок получает за него 0 баллов. Для начала игры нажмите кнопку "Старт".'],
+    'EN': ['Here is the Songbird quiz game. The rules of the game are very simple! You will be presented with 6 questions in each of which you will have to determine the breed of the bird by its singing. If the player gave the correct answer on the first attempt, his score increases by 5 points, each next attempt gives one point less, if the correct answer is given only on the last, sixth attempt, the player receives 0 points for it. To start the game, press the "Start" button.']
+  },
+  '.start': {
+    'RU': ['Старт'],
+    'EN': ['Start']
+  }
+}
